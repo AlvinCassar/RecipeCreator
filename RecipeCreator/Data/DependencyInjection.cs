420f39fa16dfe0f90e1341701a5ba4cd73c2ba28
AlvinCassar <56063874+AlvinCassar@users.noreply.github.com>
2022-02-10 14:11:21 +0100

    Initial Commit
    Created the project:
     presentation,
    data,
    domain,
    application
    
    also added first migration

---
+++ RecipeCreator/Data/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Data.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRecipe(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            return services;
+        }
+    }
+}
